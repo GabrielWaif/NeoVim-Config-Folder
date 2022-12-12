0e309baaa851b34a1c396177c6c692d3c70f7440
Gabriel Rosa <gabrielrosarcg@hotmail.com>
2022-12-11 22:44:04 -0300

    Added lsp suppor for c#

--- lua/plugins/lsp/lsp-config.lua
+++ lua/plugins/lsp/lsp-config.lua
@@ -1,16 +1,23 @@
-local nvim_lsp = require 'lspconfig'
+local nvim_lsp = require'lspconfig'
+
+local opts = { noremap=true, silent=true }
+	vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+	vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+	vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+	vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
 local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
--- Teclas Custom do lsp
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
   local bufopts = { noremap=true, silent=true, buffer=bufnr }
-
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.hover, bufopts)
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.signature_help, bufopts)
   vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
   vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
   vim.keymap.set('n', '<space>wl', function()
@@ -23,18 +30,26 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
+local lsp_flags = {
+  -- This is the default in Nvim 0.7+
+  debounce_text_changes = 150,
+}
+
+nvim_lsp.tsserver.setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
+
 -- Set up lspconfig.
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-nvim_lsp.csharp_ls.setup{} -- Lsp do Neovim com c# csharp_ls
+nvim_lsp.csharp_ls.setup{
+	on_attach = on_attach,
+  capabilities = capabilities
+} -- Lsp do Neovim com c# csharp_ls
 
 nvim_lsp.omnisharp.setup {
 	cmd = {"dotnet", "/path/to/omnisharp/OmniSharp.dll" , "--hostPID", tostring(pid) },
 	on_attach = on_attach,
   capabilities = capabilities
 } -- Lsp do Neovim com c# omnisharp
-
-nvim_lsp.tsserver.setup{
-	on_attach = on_attach,
-  capabilities = capabilities
-}
